fd08f89a1476983d737743d9f80aecbc7331e0d0
LeeSD90 <lee.doyle90@gmail.com>
2018-06-14 14:28:20 +0200

    add cell click event listeners

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const GameBoard = (() => {
 
-	let board = ['','','','','','','','',''];
+	let board = ['','','','x','','','','',''];
 
 	const clear = function () {
 		this.board = ['','','','','','','','',''];
@@ -20,7 +20,7 @@ const DisplayController = (() => {
 		let board = '';
 
 		for(i = 0; i < GameBoard.board.length; i++){
-			board += '<div class="cell col-4">X</div>'
+			board += `<div class="cell col-4">${GameBoard.board[i].toUpperCase()}</div>`
 		}
 
 		return board;
@@ -33,6 +33,35 @@ const Player = (name, side) => {
 	return { name, side }
 };
 
+const Game = (() => {
+
+	const create = function () {
+		_render();
+		_setListeners();
+	}
+
+	const _render = function () {
+		document.getElementById('board').innerHTML = DisplayController.renderBoard();
+	}
+
+	const _setListeners = function () {
+		
+		document.getElementById("board").addEventListener('click', function(e) {
+			if(e.target && e.target.matches("div.cell")) {
+				makeMove(e.target);
+			}
+		}, false);
+
+	}
+
+	const makeMove = function (cell) {
+		if(cell.innerHTML) { alert("You need to pick an empty spot on the board!"); }
+	}
+
+	return { create }
+
+})();
+
 $(document).ready(function() { 
-	document.getElementById('board').innerHTML = DisplayController.renderBoard(); 
+	Game.create() 
 });
