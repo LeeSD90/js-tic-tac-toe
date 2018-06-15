90d8bd26ecd45b522f626570d461aaa04e4dcb81
LeeSD90 <lee.doyle90@gmail.com>
2018-06-15 10:47:49 +0200

    Add win condition

--- js/script.js
+++ js/script.js
@@ -1,6 +1,6 @@
 const GameBoard = (() => {
 
-	let board = ['','','','x','','','','',''];
+	let board = ['','','','','','','','',''];
 
 	const clear = function () {
 		this.board = ['','','','','','','','',''];
@@ -62,23 +62,45 @@ const Game = (() => {
 	const _makeMove = function (cell) {
 		if(cell.innerHTML) { alert("You need to pick an empty spot on the board!"); }
 		else { 
-			var i = 0;
-			while((cell = cell.previousSibling) != null){
-				i++;
-			}
+			i = _getIndexOfCell(cell);
 			GameBoard.move(currentPlayer, i);
+			_update();
 		}
-		_update();
+	}
+
+	const _getIndexOfCell = function (cell) {
+		var i = 0;
+		while((cell = cell.previousSibling) != null){
+			i++;
+		}
+		return i;
 	}
 
 	const _update = function () {
-		currentPlayer = (currentPlayer === player1 ? player2 : player1);
-		console.log(player2)
 		_render();
+		if (_checkWin()){
+			alert(`${currentPlayer}.name has won!`);
+		}
+		else{
+			currentPlayer = (currentPlayer === player1 ? player2 : player1);
+		}
 	}
 
 	const _checkWin = function () {
+		const winConditions = [[1,2,3],[4,5,6],[7,8,9],[1,5,9],[7,5,3],[1,4,7],[2,5,8],[3,6,9]];
 
+		for(var i = 0; i < winConditions.length; i++){
+			var counter = 0;
+			for(var j = 0; j < winConditions[i].length; j++){
+				console.log(winConditions[i][j])
+				if (document.getElementById("board").childNodes[(winConditions[i][j])-1].innerHTML.toUpperCase() === currentPlayer.side.toUpperCase()){
+					
+					counter++;
+				}
+				if(counter == 3) { return true }
+			}
+		}
+		return false
 	}
 
 	return { create }
